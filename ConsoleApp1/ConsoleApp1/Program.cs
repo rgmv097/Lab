e3923959bb2a994c784791307dcb2623ae208b4c
rgmv097 <chingiznr@code.edu.az>
2022-04-15 19:17:01 +0400

    ClassWork

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           int[] arr = {4,5,6,7};
+            int NumSum = 0;
+            foreach (int i in arr)
+            {
+                if (i % 2 != 0) { NumSum += i;  }
+            }
+            Console.WriteLine(NumSum);
+
+
+        }
+    }
+}
